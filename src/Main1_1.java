47d58115dd7f22a1b3ce049526bfef4b64b13314
yeong <yeonghak2207@gmail.com>
2025-03-23 17:15:06 +0900

    First_Commit

---
+++ src/Main1_1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//코드에서 str = sc.nextLine().toLowerCase();를 사용하여 문자열은 소문자로 변환했지만, 입력받은 문자
+//chr는 소문자로 변환하지 않음. 이로 인해 대소문자 구분이 제대로 처리되지 않아 틀림
+
+
+
+public class Main1_1 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str;
+            char chr;
+
+            int count = 0;
+
+            str = sc.nextLine().toLowerCase();
+            chr = sc.next().toLowerCase().charAt(0);
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == chr) {
+                    count++;
+
+                }
+            }
+            System.out.println(count);
+        }
+}
+
---
+++ src/Main1_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main1_2 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str;
+            String s = "";
+
+
+            str = sc.nextLine();
+
+            for(int i = 0; i < str.length(); i++) {
+                if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                    s+= str.toUpperCase().charAt(i);
+                }
+                else{
+                   s+=str.toLowerCase().charAt(i);
+                }
+
+            }
+            System.out.println(s);
+
+        }
+}
+
