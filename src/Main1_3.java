b77fabe748ad4c4e13ad507f91bcafcf7c62dfe9
yeong <yeonghak2207@gmail.com>
2025-03-23 18:54:06 +0900

    First_Commit

---
+++ src/Main1_3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//이 문제의 핵심은 split.(" ")
+public class Main1_3 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str;
+            String s = "";
+            int count = 0;
+
+
+
+            str = sc.nextLine();
+            System.out.println(solution(str));
+
+            }
+    public static String solution(String str) {
+        String result = "";
+        String[] strArr = str.split(" ");
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].length() > result.length()) {
+                result = strArr[i];
+            }
+
+        }
+        return result;
+    }
+}
+
+
+
+
---
+++ src/Main1_4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class Main1_4 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int count = sc.nextInt();
+
+
+            if(!(count >= 3 && count <= 20)) {
+                return ;
+            }
+
+            for(int i = 0; i <= count; i++) {
+                String str;
+                str = sc.nextLine();
+                String reverse = "";
+                for(int j=str.length()-1 ; j>=0; j--) {
+
+                    reverse += str.charAt(j);
+                }
+
+                if(reverse != ""){
+                    System.out.println(reverse);
+                }
+
+            }
+        }
+}
+
