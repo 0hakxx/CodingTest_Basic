b16a39b8b0b5adc73ee602c46dea7f779fdf759a
yeong <yeonghak2207@gmail.com>
2025-06-16 16:28:27 +0900

    5th Commit Message

--- src/Greedy/Main96.java
+++ src/Greedy/Main96.java
@@ -2,14 +2,12 @@ package Greedy;
 
 import java.util.*;
 //트리는 정점 n개가 주어졌을 때 간선 n-1개인 자료구조이다. ==> 루프가 없다.
-//크루스칼 알고리즘(Union & Find)
+//최소 스패닝 트리 - 프림(riorityQueue 사용)
 class Edge96 implements Comparable<Edge96> {
-    int v1;   //시작 정점
-    int v2;   //목적지 종점
-    int cost; //가중치
-    public Edge96(int v1, int v2, int cost) {
-        this.v1 = v1;
-        this.v2 = v2;
+    int v;   //목적지 정점
+    int cost;
+    public Edge96(int v, int cost) {
+        this.v = v;
         this.cost = cost;
     }
     @Override
@@ -18,23 +16,42 @@ class Edge96 implements Comparable<Edge96> {
     }
 }
 public class Main96 {
-    static int[] unf;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();  //입력받는 정점 개수
-        unf = new int[n +1];
-        int m = sc.nextInt(); //입력받는 간선 개수
-        ArrayList<Edge96> edges = new ArrayList<Edge96>();
-        for(int i = 0; i < m; i++){
-            int v1 = sc.nextInt();
-            int v2 = sc.nextInt();
-            int cost = sc.nextInt();
-            edges.add(new Edge96(v1, v2, cost));
-        }
-        for (int i = 0; i <= n; i++) unf[i] = i; // 처음에는 집합의 번호를 정점 자신의 번호로 초기화
         int answer = 0;
-        Collections.sort(edges); //비용 기준 오름차순 정렬
+        int n = sc.nextInt();
+        // 방문한 정점인지 아닌지 확인하는 배열, 인덱스 번호 = 정점 번호
+        // 0이면 방문 X, 1이면 방문 O
+        int[] ch = new int[n+1];
+        ArrayList<ArrayList<Edge96>> graph = new ArrayList<ArrayList<Edge96>>();
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<Edge96>());
+        }
+        int m = sc.nextInt();
+        for(int i=0; i<m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int cost = sc.nextInt();
+            //무방향 그래프
+            graph.get(a).add(new Edge96(b, cost)); // a 정점에서 b의 정점까지 가는 비용 c
+            graph.get(b).add(new Edge96(a, cost)); // b 정점에서 a의 정점까지 가는 비용 c
+        }
+        PriorityQueue<Edge96> pq = new PriorityQueue<Edge96>(); // 최소 비용 기준으로 poll
+        // 정점 1에서 시작
+        pq.add(new Edge96(1,0));
 
+        while(!pq.isEmpty()) {
+            Edge96 tmp = pq.poll();
+            int ev = tmp.v; // ev = 도착 정점(end vex)
+            if(ch[ev]==0) { // 방문하지 않은 정점이라면
+                ch[ev] = 1; // 방문 처리
+                answer += tmp.cost;
+                for(Edge96 ob : graph.get(ev)) {
+                    // 방문하지 않은 정점만 큐에 추가
+                    if(ch[ob.v]==0) {pq.offer(new Edge96(ob.v, ob.cost));}
+                }
+            }
+        }
         System.out.println(answer);
     }
 }
